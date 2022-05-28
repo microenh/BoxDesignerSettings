11a65155db126a26669cd0bf02df4f7f98e19137
Mark Erbaugh <mark.election@gmail.com>
2022-05-28 18:56:22 -0400

    Initial Commit

---
+++ BoxDesignerSettings/BoxDesignerSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxDesignerSettingsApp.swift
+//  BoxDesignerSettings
+//
+//  Created by Mark Erbaugh on 5/28/22.
+//
+
+import SwiftUI
+
+@main
+struct BoxDesignerSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
